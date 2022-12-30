d6a44142b1603e7d2dc81c46d896d69948456f67
RyanAditya <105885848+ReffDessn@users.noreply.github.com>
2022-12-30 13:44:52 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = `https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg`
+    let img2 = `https://telegra.ph/file/0e14e6462664f19e2c7fe.jpg`
     return conn.sendButton(m.chat, 'ᴅᴏɴᴀᴛᴇ ғᴏʀ ʙᴏᴛ\nᴘᴍ ᴏᴡɴᴇʀ ᴋᴀᴋ ᴋᴀʟᴀᴜ ᴍᴀᴜ ᴅᴏɴᴀᴛᴇ\n\n➪ ᴅᴀɴᴀ : 082350496532\n➪ ᴏᴠᴏ : 082350496532\n➪ ɢᴏᴘᴀʏ : 082252285143\n➪ ǫʀɪs ✓\n« ʜᴀsɪʟ ᴅᴏɴᴀᴛᴇ ᴀᴋᴀɴ ᴅɪɢᴜɴᴀᴋᴀɴ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʟɪ ᴋᴇʙᴜᴛᴜʜᴀɴ ʙᴏᴛ sᴇᴘᴇʀᴛɪ ᴀᴘɪᴋᴇʏ ᴅᴀɴ ʀᴅᴘ ᴀɢᴀʀ ᴛᴇᴛᴀᴘ ᴏɴ 24 ᴊᴀᴍ »\nᴛǫ ғᴏʀ ᴅᴏɴᴀᴛᴇ...\nxʏʟᴏɪᴅ ʙᴏᴛ  :v', 'ᴛǫ ғᴏʀ ᴅᴏɴᴀᴛᴇ', img2, [["ᴍᴇɴᴜ", usedPrefix + "menu"], ["ᴏᴡɴᴇʀ", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
